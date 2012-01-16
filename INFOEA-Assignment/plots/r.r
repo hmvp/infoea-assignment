e54cd91aba721d7e5352a7a1e418436870bb6129
tibod <gitorious@timmybankers.nl>
2012-01-16 22:49:46 +0100

    better plots

--- INFOEA-Assignment/plots/r.r
+++ INFOEA-Assignment/plots/r.r
@@ -1,8 +1,22 @@
-c <- csv.get('test.csv', sep=';')
-ils <- c[c$Heuristic=='ILS',]
-gls <- c[c$Heuristic=='GLS',]
-agls <- c[c$Heuristic=='AGLS',]
+require(Hmisc)
+require(data.table)
+require(ggplot2)
 
-plot(Fitness~Index,data=ils, type='l')
-lines(gls$Index,gls$Fitness, col=2)
-lines(agls$Index,agls$Fitness, col=3)
+data <- csv.get('test.csv', sep=';')
+dataTable <- data.table(c) 
+grouped <- dataTable[, list(Instance=Instance, Fitness=mean(Fitness)), by=list(Problem, Heuristic, Index)]
+
+instance <- 0
+problem <- 'SAT'
+
+heuristics = c('ILS', 'GLS', 'AGLS')
+nHeuristics = length(heuristics)
+
+p <- ggplot(grouped, aes(Index, Fitness, colour=factor(Heuristic)))
+
+for (h in heuristics) {
+  heur <- grouped[Heuristic==h,][Instance==instance,][Problem==problem,]
+  p <- p + geom_line(aes(Index, Fitness), data=heur)
+}
+
+p <- p + opts(title = paste(problem, ", instance ", instance, sep=""))
