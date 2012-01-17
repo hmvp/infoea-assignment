46a5b9acfd529d2a642a03b199131a94a2f99d6a
Hiram <git@hmvp.nl>
2012-01-17 16:12:26 +0100

    fixed some errors in R script

--- INFOEA-Assignment/plots/r.r
+++ INFOEA-Assignment/plots/r.r
@@ -3,8 +3,11 @@ require(data.table)
 require(ggplot2)
 
 data <- csv.get('test.csv', sep=';')
-dataTable <- data.table(c) 
-grouped <- dataTable[, list(Instance=Instance, Fitness=mean(Fitness)), by=list(Problem, Heuristic, Index)]
+
+# eerste indexwaarde eruitgooien, dit maakt sommige grafiekjes duidelijker
+data <- subset(data, Index > 0)
+dataTable <- data.table(data) 
+grouped <- dataTable[, list(Fitness=mean(Fitness)), by=list(Problem, Heuristic, Instance, Index)]
 
 instance <- 0
 problem <- 'SAT'
@@ -15,7 +18,7 @@ nHeuristics = length(heuristics)
 p <- ggplot(grouped, aes(Index, Fitness, colour=factor(Heuristic)))
 
 for (h in heuristics) {
-  heur <- grouped[Heuristic==h,][Instance==instance,][Problem==problem,]
+  heur <- grouped[Heuristic==h][Instance==instance][Problem==problem]
   p <- p + geom_line(aes(Index, Fitness), data=heur)
 }
 
