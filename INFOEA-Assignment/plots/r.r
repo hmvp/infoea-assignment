9651157113287ee18ec7da9ba91fcf0f17dfdf25
tibod <gitorious@timmybankers.nl>
2012-01-18 22:06:56 +0100

    added nicer output for all instances and problems

--- INFOEA-Assignment/plots/r.r
+++ INFOEA-Assignment/plots/r.r
@@ -1,25 +1,55 @@
 require(Hmisc)
 require(data.table)
 require(ggplot2)
+require(gridExtra)
 
-data <- csv.get('test.csv', sep=';')
+#loadData <- function(){
+inputFileName = 'runtrace-2012-01-16-0.csv'
+data <- csv.get(inputFileName, sep=';')
 
 # eerste indexwaarde eruitgooien, dit maakt sommige grafiekjes duidelijker
 data <- subset(data, Index > 0)
 dataTable <- data.table(data) 
 grouped <- dataTable[, list(Fitness=mean(Fitness)), by=list(Problem, Heuristic, Instance, Index)]
 
-instance <- 0
-problem <- 'SAT'
-
-heuristics = c('ILS', 'GLS', 'AGLS')
-nHeuristics = length(heuristics)
-
-p <- ggplot(grouped, aes(Index, Fitness, colour=factor(Heuristic)))
-
-for (h in heuristics) {
-  heur <- grouped[Heuristic==h][Instance==instance][Problem==problem]
-  p <- p + geom_line(aes(Index, Fitness), data=heur)
+getInstancesPlot <- function(problem='SAT'){
+	instances <- unique(grouped$Instance)
+	problems <- unique(grouped$Problem)
+	heuristics <- unique(grouped$Heuristic)
+	nHeuristics = length(heuristics)
+	
+	plots <- list()
+	
+	for(i in instances) {
+	  p <- ggplot(grouped, aes(Index, Fitness, colour=factor(Heuristic)))
+	
+		for (h in heuristics) {
+		  heur <- grouped[Heuristic==h][Instance==i][Problem==problem]
+		  p <- p + geom_line(aes(Index, Fitness), data=heur)
+		}
+	plots[[toString(i)]] <- p + opts(legend.position="none")
+	}
+	
+	# Legend
+	leg <- ggplotGrob(p + opts(keep="legend_box") + labs(colour = "Heuristics"))
+	## one needs to provide the legend with a well-defined width
+	legend=gTree(children=gList(leg), cl="legendGrob")
+	widthDetails.legendGrob <- function(x) unit(2.5, "cm")
+	plots[["legend"]] <- legend
+	plots[["main"]] <- problem
+	
+	return(plots)
 }
 
-p <- p + opts(title = paste(problem, ", instance ", instance, sep=""))
+plotInstances <- function(problem='SAT'){
+	do.call(grid.arrange, getInstancesPlot(problem))
+}
+
+outputToPdf <- function() {
+	problems <- unique(grouped$Problem)
+	pdf(paste(inputFileName,'.pdf',sep=''))
+	for(p in problems){
+		do.call(grid.arrange, getInstancesPlot(p))
+	}
+	dev.off()
+}
